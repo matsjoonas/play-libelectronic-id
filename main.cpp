2bf27a928b0f588dc5a4765b7f1caee2984826c8
matsjoonas <matsjoonas@gmail.com>
2022-05-30 17:54:43 +0300

    Print out certificates

--- main.cpp
+++ main.cpp
@@ -11,49 +11,24 @@
 using namespace pcsc_cpp;
 using namespace electronic_id;
 using pcsc_cpp::string_t;
-/*
-CardCertificateAndPinInfo getCertificateWithStatusAndInfo(const CardInfo::ptr& card,
-                                                          const CertificateType certificateType)
+
+QString getCertificate(const CardInfo::ptr& card,
+                       const CertificateType certificateType)
 {
     const auto certificateBytes = card->eid().getCertificate(certificateType);
 
     auto certificateDer = QByteArray(reinterpret_cast<const char*>(certificateBytes.data()),
                                      int(certificateBytes.size()));
     auto certificate = QSslCertificate(certificateDer, QSsl::Der);
-    if (certificate.isNull()) {
-        THROW(SmartCardChangeRequiredError,
-              "Invalid certificate returned by electronic ID " + card->eid().name());
-    }
 
-    auto certInfo = CertificateInfo {certificateType,
-                                     certificate.expiryDate() < QDateTime::currentDateTimeUtc(),
-                                     certificate.effectiveDate() > QDateTime::currentDateTimeUtc(),
-                                     certificate.subjectInfo(QSslCertificate::CommonName).join(' '),
-                                     certificate.issuerInfo(QSslCertificate::CommonName).join(' '),
-                                     certificate.effectiveDate().date().toString(Qt::ISODate),
-                                     certificate.expiryDate().date().toString(Qt::ISODate)};
-    auto pinInfo =
-            PinInfo {certificateType.isAuthentication() ? card->eid().authPinMinMaxLength()
-                                                        : card->eid().signingPinMinMaxLength(),
-                     certificateType.isAuthentication() ? card->eid().authPinRetriesLeft()
-                                                        : card->eid().signingPinRetriesLeft(),
-                     card->eid().smartcard().readerHasPinPad()};
-    if (pinInfo.pinRetriesCount.first == 0) {
-        pinInfo.pinIsBlocked = true;
-    }
-
-    return {card};
+    return certificate.toText();
 }
- */
 
 int main()
 {
     std::cout << "*** START ***" << std::endl;
 
-    auto certificateDer = QByteArray(1, 1);
-    auto certificate = QSslCertificate(certificateDer, QSsl::Der);
 
-    /*
     auto cardList = electronic_id::availableSupportedCards();
     if (cardList.empty()) {
         throw std::logic_error("test::autoSelectSupportedCard(): No smart cards attached");
@@ -62,13 +37,22 @@ int main()
     const auto cardInfo = cardList[0];
     const std::wstring reader_name = cardInfo->reader().name;
     const std::string eid_name = cardInfo->eid().name();
-
     std::wcout << "Reader: " << reader_name << std::endl;
     std::cout << "Card: " << eid_name << std::endl;
+    std::cout << std::endl;
 
-    const auto certInfo = getCertificateWithStatusAndInfo(cardInfo, CertificateType::AUTHENTICATION);
+
+    std::cout << "*** Signing cert start ***" << std::endl;
+    const QString signingCert = getCertificate(cardInfo, CertificateType::SIGNING);
+    std::cout << signingCert.toStdString() << std::endl;
+    std::cout << "*** Signing cert end ***" << std::endl;
+
+    std::cout << "*** Auth cert start ***" << std::endl;
+    const QString authCert = getCertificate(cardInfo, CertificateType::AUTHENTICATION);
+    std::cout << authCert.toStdString() << std::endl;
+    std::cout << "*** Auth cert end ***" << std::endl;
+
 
     std::cout << "*** END ***" << std::endl;
-     */
     return  0;
 }
